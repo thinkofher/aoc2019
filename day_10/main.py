db4e5077be3d1e0ae5d3b48e9e4d39a9e9da6265
thinkofher <piaskupiasku@gmail.com>
2019-12-10 19:03:36 +0100

    refactored code

--- day_10/main.py
+++ day_10/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # Beniamin Dudek <beniamin.dudek@yahoo.com, github.com/thinkofher>
 import argparse
-from typing import List, NamedTuple, Set, Dict
+from typing import List, NamedTuple, Set, Dict, Tuple
+from math import acos, pi
 
 ASTEROID = "#"
 EMPTY = "."
+PRECISION = 3
 
 
 class Point(NamedTuple):
@@ -18,15 +20,58 @@ class Vector(NamedTuple):
 
     def to_unit(self) -> 'Vector':
         return self.__class__(
-            round(self.x / self.length(), 3),
-            round(self.y / self.length(), 3))
+            round(self.x / self.length(), PRECISION),
+            round(self.y / self.length(), PRECISION))
 
     def length(self) -> float:
-        return round((self.x**2 + self.y**2)**0.5, 3)
+        return round((self.x**2 + self.y**2)**0.5, PRECISION)
+
+    @property
+    def angle_from_origin(self) -> float:
+        origin_x = -1
+        origin_y = 0
+
+        return round(
+            acos((origin_x * self.x + origin_y * self.y)/(self.length()))
+            * (180/pi), PRECISION)
 
     @classmethod
     def from_two_points(cls, p1: Point, p2: Point) -> 'Vector':
         return cls(float(p2.x - p1.x), float(p2.y - p1.y))
+
+
+def parse_asteroids(asteroid_map: List[List[str]]) -> Set[Point]:
+    asteroids = set()
+    for y, verse in enumerate(asteroid_map):
+        for x, value in enumerate(verse):
+            if value == ASTEROID:
+                asteroids.add(Point(x, y))
+    return asteroids
+
+
+def calculate_vectors(asteroids: Set[Point]) -> Dict[Point, Set[Vector]]:
+    vectors = {}
+    for base in asteroids:
+        for asteroid in (asteroids - set([base])):
+            try:
+                vectors[base].add(
+                    Vector.from_two_points(base, asteroid).to_unit())
+            except KeyError:
+                vectors[base] = set(
+                    [Vector.from_two_points(base, asteroid).to_unit()])
+    return vectors
+
+
+def find_best_base(vectors_map: Dict[Point, Set[Vector]]) -> Tuple[Point, int]:
+    detected_asteroids: Dict[Point, int] = {
+        point: len(vec_set) for point, vec_set in vectors_map.items()
+    }
+    best_result = max(detected_asteroids.values())
+    points_by_detected_asteroids = {
+        value: key for key, value in detected_asteroids.items()
+    }
+
+    return points_by_detected_asteroids[best_result], best_result
 
 
 if __name__ == "__main__":
@@ -43,24 +88,8 @@ if __name__ == "__main__":
         for line in f.readlines():
             asteroid_map += [list(line.strip("\n"))]
 
-    asteroids: Set[Point] = set()
-    for y, verse in enumerate(asteroid_map):
-        for x, value in enumerate(verse):
-            if value == ASTEROID:
-                asteroids.add(Point(x, y))
+    asteroids = parse_asteroids(asteroid_map)
+    vectors = calculate_vectors(asteroids)
 
-    vectors: Dict[Point, Set[Vector]] = {}
-
-    for base in asteroids:
-        for asteroid in (asteroids - set([base])):
-            try:
-                vectors[base].add(
-                    Vector.from_two_points(base, asteroid).to_unit())
-            except KeyError:
-                vectors[base] = set(
-                    [Vector.from_two_points(base, asteroid).to_unit()])
-
-    detected_asteroids = {
-        point: len(vec_set) for point, vec_set in vectors.items()
-    }
-    print(max(detected_asteroids.values()))
+    base, best_result = find_best_base(vectors)
+    print(best_result)
